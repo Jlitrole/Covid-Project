aeb509b03661c87f04cd3febac30368d9039f7f1
Joe Laurle <Josue.laurole@gmail.com>
2020-06-22 01:23:31 -0400

    Update Covdcheck.py

--- Covdcheck.py
+++ Covdcheck.py
@@ -4,4 +4,15 @@ from selenium import webdriver
 from Beautifulsoup4 import Beautifulsoup4
 import pandas as pd
 
-content = driver.page
+for url in ['https://covidtracking.com/api']:
+    try:
+        response = request.get('https://covidtracking.com/api')
+
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        print('Success!')
